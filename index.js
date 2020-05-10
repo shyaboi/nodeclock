a65d14b111e9466feb84a74df7005838141abf75
Ian Sears <iansears@Ians-MBP.domain>
2020-05-10 13:11:16 -0700

    back to basics

--- index.js
+++ index.js
@@ -2,34 +2,51 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
+var ghpages = require('gh-pages');
+
 // var moment = require('moment');
 // var now = moment();
 
 // moment().format();
 
+let clock = 0;
 
+function clockTic() {
+  clock++;
+  // console.log(clock);
+}
 
+function reload() {
+  
+}
 
+function html() {
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-    // res.send(`<span id='clk'>${clock}</span> 
+    // res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.send(`<span id='clk'>${clock}</span> 
     
-    // <script>
+    <script>
             
            
             
         
-   
+    function re() {
+    location.reload();   
+    }
     
-    // setInterval(() => {
-    //     re()
-    //     console.log('ok')
-    // }, 1000);
+    setInterval(() => {
+        re()
+        console.log('ok')
+    }, 1000);
     
-    // </script>`);
+    </script>`);
   });
+}
 
-
+setInterval(() => {
+  clockTic();
+  html();
+}, 1000);
 
 // app.use(express.static(path.join(__dirname, "public")));
 
